1f10741f9d0b7dc51dd20666b13763deb02ec7bf
Javier Rivera <riverafuentesj@yahoo.es>
2023-05-06 18:01:25 -0600

    first commit

---
+++ src/components/Practica2.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+let num:number = 16;
+
+//if and else corriente
+if (num >= 7 && num <=10  ) {
+    console.log("El numero no es igual")
+}
+else{ 
+    console.log("El numero no es igual")
+}
+
+// and &&
+// or ||
+// condicion ternaria
+num >= 10 && num <=19 ? console.log("condicion corecta") : console.log("condicion incorrecta")
+
+// ternaria con and integrado
+
+num >=10 && num <19 && console.log("condicion 1")
+
+
+</script>
+
+<template>
+<h1 v-if="num==10">Practica 2</h1>
+<h1 v-else>Practica 2</h1>
+<h1 v-show="num == 16">Ah facilito donde le subo la dificultad a estos bots</h1>
+
+</template>
+
+<style>
+</style>
---
+++ src/components/Prueba.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+let valor = ref<number>(0);
+
+let arreglo =ref<number[]>([1,2,3,4,5,6]) ;
+
+function AgregarNumero() {
+
+    arreglo.value.push(arreglo.value.length + 1)
+}
+
+function Nuevo(){
+arreglo.value.map((numero)=>{console.log(`${numero} * ${valor.value} =  ${numero*valor.value}` )})
+
+}
+
+function filtro(){
+   const par = arreglo.value.filter((numero)=>numero%2 === 0 )
+   return par
+}
+
+
+function multimpares(){
+    arreglo.value.filter((numero)=>numero%2 !== 0).map((numero)=>{console.log(`${numero} * ${valor.value} =  ${numero*valor.value}` )})
+
+}
+
+function multpares(){
+    filtro().map((numero)=>{console.log(`${numero} * ${valor.value} =  ${numero*valor.value}` )})
+}
+</script>
+<template>
+    <input type="text" v-model="valor"/>
+    <button @click="AgregarNumero">change</button>
+    
+<ul>
+    <li v-for="numero in arreglo" >
+
+        {{ numero }}
+    
+    </li>
+</ul>
+
+<button @click="Nuevo">nuevo</button>
+
+<button @click="filtro">Numero Par</button>
+
+<button @click="multimpares">multiplcar pares</button>
+
+</template>
